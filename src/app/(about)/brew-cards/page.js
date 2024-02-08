5caf15ac018c96d1f9abed7f82bb0f267bc4b023
Austin Amento <76490816+puginator@users.noreply.github.com>
2024-02-08 09:12:07 -0800

    updated data to properly display

--- src/app/(about)/brew-cards/page.js
+++ src/app/(about)/brew-cards/page.js
@@ -1,6 +1,5 @@
-import AboutCoverSection from "@/src/components/About/AboutCoverSection";
-import Skills from "@/src/components/About/Skills";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata = {
   title: "Brew Cards",
@@ -8,11 +7,41 @@ export const metadata = {
 };
 
 export default function BrewCards() {
-  //Fetch brewcards json
+  // Fetch brew cards json
+
   const data = require("@/src/data/cardData.json");
-  console.log(data);
+  //console.log(data);
+  const cards = data.map((card) => (
+    <div key={card.name} className="w-full md:max-w-[450px]">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="mt-8 font-semibold text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+            {card.name}
+          </h1>
+          <div className="flex flex-col items-center justify-center">
+            <Image src={card.image} alt={card.name} width={200} height={200} className="aspect-square object-contain" />
+            <div className="flex flex-col items-center justify-center">
+              <h2 className="mt-8 font-semibold text-lg self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+                {card.description}
+              </h2>
+              <h2 className="mt-8 font-semibold text-lg self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+                {card.recipe}
+              </h2>
+              <h2 className="mt-8 font-semibold text-lg self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+                {Object.values(card.instructions).map((instruction, index) => (
+                  <p key={index}>{instruction}</p>
+                ))}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <>
+      <div className="flex flex-wrap justify-center gap-4 p-8">{cards}</div>
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
         Have some coffee you want me to try?<br></br> Reach out to me 📞{" "}
